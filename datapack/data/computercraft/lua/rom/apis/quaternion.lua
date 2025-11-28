196ac7459fb102cfedb48de4efe6131e4c67fb6e
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-11-28 16:35:07 -0600

    Updated `quaternion.lua` documentation to fix reference to `Matrix.cs`

--- datapack/data/computercraft/lua/rom/apis/quaternion.lua
+++ datapack/data/computercraft/lua/rom/apis/quaternion.lua
@@ -8,7 +8,7 @@
 --
 -- Special thanks to getItemFromBlock and Shlomo for sharing their own quaternion handling code.
 --
--- If you are interested in using [CCSharp][ccsharp], here is the compatible [Matrix.cs][ccsharp-quaternion] file.
+-- If you are interested in using [CCSharp][ccsharp], here is the compatible [Quaternion.cs][ccsharp-quaternion] file.
 --
 -- [ccsharp]: https://github.com/monkeymanboy/CCSharp
 -- [ccsharp-quaternion]: https://github.com/TechTastic/Advanced-Math/blob/main/CCSharp/src/CCSharp/AdvancedMath/Quaternion.cs
